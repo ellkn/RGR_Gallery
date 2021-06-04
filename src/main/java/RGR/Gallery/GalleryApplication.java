49ad904eac4d5da878720b5844f19853e5979656
Нелли Камалян <62396078+ellkn@users.noreply.github.com>
2021-06-05 01:37:57 +0400

    application update

--- src/main/java/RGR/Gallery/GalleryApplication.java
+++ src/main/java/RGR/Gallery/GalleryApplication.java
@@ -1,54 +1,13 @@
-package RGR.Gallery;
+package com.example.gallery;
 
-import org.ocpsoft.rewrite.servlet.RewriteFilter;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.datasource.init.DataSourceInitializer;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
-import javax.faces.webapp.FacesServlet;
-import javax.servlet.DispatcherType;
-import javax.sql.DataSource;
-import java.util.EnumSet;
-
-@EnableAutoConfiguration
-@ComponentScan({"RGR.Gallery"})
-public class GalleryApplication extends SpringBootServletInitializer {
+@SpringBootApplication
+public class GalleryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GalleryApplication.class, args);
     }
 
-    @Bean
-    public ServletRegistrationBean servletRegistrationBean() {
-        FacesServlet servlet = new FacesServlet();
-        return new ServletRegistrationBean(servlet, "*.jsf");
-    }
-
-    @Bean
-    public FilterRegistrationBean rewriteFilter() {
-        FilterRegistrationBean rwFilter = new FilterRegistrationBean(new RewriteFilter());
-        rwFilter.setDispatcherTypes(EnumSet.of(DispatcherType.FORWARD, DispatcherType.REQUEST,
-                DispatcherType.ASYNC, DispatcherType.ERROR));
-        rwFilter.addUrlPatterns("/*");
-        return rwFilter;
-    }
-
-    @Bean
-    public DataSourceInitializer dataSourceInitializer(@Qualifier("dataSource") final DataSource dataSource) {
-        ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator();
-        resourceDatabasePopulator.addScript(new ClassPathResource("/database/data.sql"));
-        DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
-        dataSourceInitializer.setDataSource(dataSource);
-        dataSourceInitializer.setDatabasePopulator(resourceDatabasePopulator);
-        return dataSourceInitializer;
-    }
 }
