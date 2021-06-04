8d99757856b7e118e5d52d901f2bbe10224c89f8
ell_kn <nelli.kamalyan@mail.ru>
2021-06-04 19:33:26 +0400

    Delete old files

--- src/main/java/RGR/Gallery/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package RGR.Gallery.exception;
-
-public class UserNotFoundException extends  RuntimeException{
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/RGR/Gallery/repository/CommentsRepository.java
+++
@@ -1,7 +0,0 @@
-package RGR.Gallery.repository;
-
-import RGR.Gallery.domain.Comments;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentsRepository extends JpaRepository<Comments, Long> {
-}
